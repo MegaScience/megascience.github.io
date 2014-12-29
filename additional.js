079d2578958b2af937ec270ff90e50957304cb57
MegaScience <megascientifical@yahoo.com>
2014-12-29 15:19:27 -0500

    Create additional.js

---
+++ additional.js
@@ -0,0 +1,15 @@
+var name = document.getElementById("textArea").value;
+var imageArea = document.getElementById("imageArea");
+var newElement, newImage, imageAreaDivs;
+
+function addImage() {
+    if(name === null || name.trim() !== "") return;
+    newElement = document.createElement("div");
+    newImage = document.createElement("img");
+    newImage.src = "http://api.skype.com/users/" + name + "/profile/avatar";
+    newElement.appendChild(newImage);
+    newElement.innerHTML += name;
+    imageAreaDivs = imageArea.getElementsByTagName("div");
+    if(imageAreaDivs.length > 24) imageAreaDivs.last().remove();
+    imageArea.appendChild(newElement);
+}
