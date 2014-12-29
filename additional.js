bffdebc1f4265840e89bab15eea61e136aa8cbeb
MegaScience <megascientifical@yahoo.com>
2014-12-29 16:05:15 -0500

    Update additional.js

--- additional.js
+++ additional.js
@@ -1,15 +1,21 @@
 var imageArea = document.getElementById("imageArea");
-var newElement, newImage, imageAreaDivs;
+var newDIV, newImage, newURL, curURL, divList;
 
 function addImage() {
     var name = document.getElementById("textArea").value;
     if(name === null || name.trim() === "") return;
-    newElement = document.createElement("div");
+    newDIV = document.createElement("div");
+    newURL = document.createElement("a");
     newImage = document.createElement("img");
-    newImage.src = "http://api.skype.com/users/" + name.trim() + "/profile/avatar";
-    newElement.appendChild(newImage);
-    newElement.innerHTML += "<br>" + name;
-    imageAreaDivs = imageArea.getElementsByTagName("div");
-    if(imageAreaDivs.length > 24) imageAreaDivs[imageAreaDivs.length - 1].remove();
-    imageArea.appendChild(newElement);
+    curURL = "http://api.skype.com/users/" + name.trim() + "/profile/avatar";
+    newURL.href = curURL;
+    newImage.src = curURL;
+    newImage.height = "96px";
+    newImage.width = "96px";
+    newURL.appendChild(newImage);
+    newDIV.appendChild(newURL);
+    newDIV.innerHTML += "<br>" + name;
+    divList = imageArea.getElementsByTagName("div");
+    if(divList.length > 53) divList[divList.length - 1].remove();
+    imageArea.appendChild(newDIV);
 }
