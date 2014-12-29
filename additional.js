980c1c5379b573e73a513b2b646d786721497c17
MegaScience <megascientifical@yahoo.com>
2014-12-29 15:32:42 -0500

    Update additional.js

--- additional.js
+++ additional.js
@@ -1,15 +1,14 @@
-var name = document.getElementById("textArea");
 var imageArea = document.getElementById("imageArea");
-var newElement, newImage, imageAreaDivs, nameString;
+var newElement, newImage, imageAreaDivs;
 
 function addImage() {
-    nameString = name.value;
-    if(nameString === null || nameString.trim() === "") return;
+    var name = document.getElementById("textArea").value;
+    if(name === null || name.trim() === "") return;
     newElement = document.createElement("div");
     newImage = document.createElement("img");
-    newImage.src = "http://api.skype.com/users/" + nameString.trim() + "/profile/avatar";
+    newImage.src = "http://api.skype.com/users/" + name.trim() + "/profile/avatar";
     newElement.appendChild(newImage);
-    newElement.innerHTML += "<br>" + nameString;
+    newElement.innerHTML += "<br>" + name;
     imageAreaDivs = imageArea.getElementsByTagName("div");
     if(imageAreaDivs.length > 24) imageAreaDivs.last().remove();
     imageArea.appendChild(newElement);
